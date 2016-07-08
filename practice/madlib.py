bfbb0615a3db776cc480010f865d7bf29c8d1814
Eric Chase <echase6@gmail.com>
2016-07-08 12:17:14 -0700

    initial commit

---
+++ practice/madlib.py
@@ -0,0 +1,16 @@
+"""
+
+"""
+#1. Setup
+
+#2. Input
+print('Please provide two nouns and an adjective:')
+noun_1 = input()
+noun_2 = input()
+adjective = input()
+
+#3. Transform
+out_str = 'The ' + noun_1 + ' jumped over the ' + adjective + ' ' + noun_2
+
+#4. Output
+print(out_str)
---
+++ practice/wall-painting_sadv.py
@@ -0,0 +1,36 @@
+"""
+Individual Project to calculate the cost of painting a wall
+Inputs:  Wall height, width and cost per gallon of painting
+Output:  cost
+Optional Output:  quantity of gallons of paint to buy
+"""
+
+#1. Setup
+import math
+COVERS = 400  # Gallons per square foot
+sum_area = 0
+
+#2. Input
+print('Let me calculate how much your painting project will cost.')
+print('How many walls do you want to paint')
+num_walls = int(input())
+for i in range(num_walls):
+    print('What is the width of the wall ' + str(i) + '?')
+    width = float(input())
+    print('What is the height of the wall ' + str(i) + '?')
+    height = float(input())
+    sum_area += width * height
+print('How much does a gallon of paint cost?')
+price_gallon = float(input())
+print('How many coats do you want to apply?')
+num_coats = int(input())
+
+#3. Transform
+num_used_gallons = num_coats * sum_area / COVERS
+num_gallons = math.ceil(num_used_gallons)
+cost = int(num_gallons * price_gallon * 100) / 100
+
+#4. Output
+print('You will use ' + str(num_used_gallons) + ' gallons of paint.')
+print('You should buy ' + str(num_gallons) + ' gallons of paint.')
+print('It will cost you $' + str(cost))
