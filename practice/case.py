cf9559258f2824d16ee364fdee7d04f2df2ab3f0
Eric Chase <echase6@gmail.com>
2016-07-13 15:45:31 -0700

    initial commit

---
+++ practice/case.py
@@ -0,0 +1,59 @@
+""" Case conversion
+This is an individual project by Eric Chase, 7/13/16
+
+"""
+
+
+def get_input_word():
+    """ Get user word, requested to be changed. """
+    return input('What is your word? ')
+
+
+def get_input_case():
+    """ Get input case. """
+    return input('What is it\'s case, snake or camel? ')
+
+
+def convert_to_snake(word, in_case):
+    """ Convert word to snake case from input case.
+
+    >>> convert_to_snake('snake_case', 'snake')
+    'snake_case'
+    >>> convert_to_snake('CamelCase', 'camel')
+    'camel_case'
+    """
+    out_word = []
+    if in_case == 'snake':
+        out_word = word
+    elif in_case == 'camel':
+        out_word = word[0].lower()
+        out_word += ''.join([d.replace(d, '_' + d.lower())
+                             if d.isupper() else d for d in word[1:]])
+    else:
+        print('Invalid case.')
+    return out_word
+
+
+def convert_from_snake(word, out_case):
+    """ Convert word to desired case, from snake case
+
+    >>> convert_from_snake('snake_case', 'camel')
+    'SnakeCase'
+    """
+    if out_case == 'snake':
+        out_word = word
+    elif out_case == 'camel':
+        out_word = ''.join([w.capitalize() for w in word.split('_')])
+    return out_word
+
+
+def main():
+    input_word = get_input_word()
+    input_case = get_input_case()
+    for output_case in ['snake', 'camel']:
+        snake_word = convert_to_snake(input_word, input_case)
+        converted_word = convert_from_snake(snake_word, output_case)
+        print('{} in {} is {}'.format(input_word, output_case, converted_word))
+
+
+main()
