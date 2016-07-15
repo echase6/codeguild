e07eab3a26de1f678ff65809672f5ed110415236
Eric Chase <echase6@gmail.com>
2016-07-15 15:52:00 -0700

    added day of year comprehension

--- practice/rain.py
+++ practice/rain.py
@@ -3,6 +3,7 @@
 Accesses City of Portland rain total database and returns various information
 about rain totals.
 """
+from operator import itemgetter
 
 def get_filename():
     """Return the filename of the database."""
@@ -36,7 +37,11 @@ def find_wettest_stats(totals):
 
 
 def calc_yearly_total_stats(totals):
-    """Return a list of lists of years and sum amounts."""
+    """Return a list of lists of years and sum amounts.
+
+    >>> calc_yearly_total_stats([['30-MAR-2016', 1], ['29-MAR-2016', 5], ['30-MAR-2015', 0], ['29-MAR-2015', 5]])
+    [['2015', 5], ['2016', 6]]
+    """
     years = set()
     yearly_totals = []
     for date, amount in totals:
@@ -45,7 +50,34 @@ def calc_yearly_total_stats(totals):
     for year in years:
         yearly_totals += [[year, sum([amt for date, amt in totals
                                    if date.split('-')[2] == year])]]
-    return yearly_totals
+    return sorted(yearly_totals)  # sorted only to make the doctest work
+
+
+def find_daily_avg(totals):
+    days = set()
+    daily_avg = []
+    for date, amount in totals:
+        day = '-'.join(date.split('-')[0:2])
+        days.add(day)
+    daily_avg = []
+    for day in days:
+        daily_stats = ([amt for date, amt in totals
+                         if '-'.join(date.split('-')[0:2]) == day])
+        daily_avg += [[day, (sum(daily_stats) / len(daily_stats))]]
+    return sorted(daily_avg, key=itemgetter(1), reverse = True)
+
+
+def find_wettest_doy(averages):
+    return sorted(averages, key=itemgetter(1), reverse = True)[0]
+
+
+def find_avg_amt_on_day(date, averages):
+    averages_dict = dict(averages)
+    return averages_dict[date]
+
+
+def get_inquiry_date():
+    return input('What date would you like to check (DD=MMM): ')
 
 
 def output_most_rain_stats(day, day_amt, year, year_amt):
@@ -60,12 +92,15 @@ def main():
     wettest_day, wettest_day_amt = find_wettest_stats(daily_totals)
     yearly_totals = calc_yearly_total_stats(daily_totals)
     wettest_year, wettest_year_amt = find_wettest_stats(yearly_totals)
-    # wettest_avg_day = find_wettest_avg_day(daily_totals)
-    # inquiry_date = get_inquiry_date()
-    # average_amount_on_day = find_avg_amt_on_day(inquiry_date)
+    daily_averages = find_daily_avg(daily_totals)
+    wettest_doy = find_wettest_doy(daily_averages)
+    print('Wettest day of the year is {} with {:.2f}"'.format(wettest_doy[0], wettest_doy[1] / 100))
+    inquiry_date = get_inquiry_date()
+    avg_amt_on_day = find_avg_amt_on_day(inquiry_date, daily_averages)
+    print('Average rainfall on {} is {:.2f}"'.format(inquiry_date, avg_amt_on_day / 100))
     output_most_rain_stats(wettest_day, wettest_day_amt, wettest_year, wettest_year_amt)
     return
 
 
 if __name__ == '__main__':
-    main()
+   main()
