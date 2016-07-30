efbc26989a44e202b46321db4cee11cc1b533b78
Eric Chase <echase6@gmail.com>
2016-07-30 09:05:46 -0700

    added better docstrings

--- practice/tic_tac_toe/list_board.py
+++ practice/tic_tac_toe/list_board.py
@@ -1,4 +1,10 @@
-"""Implements Tic-Tac-Toe board using list of lists."""
+"""Implements Tic-Tac-Toe board using list of lists.
+
+Methods:
+    calc_winner(self) returns the token (as a string) if there is a winner.
+      it uses hidden functions _calc_winner_row_col() and _calc_winner_diag()
+    str(self) will return a string for pretty-printing the board.
+"""
 
 TOKENS = ['X', 'O']
 
