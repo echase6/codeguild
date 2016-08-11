244cc23e902661cfef48bc38b49b84eca97786af
Eric Chase <echase6@gmail.com>
2016-08-11 10:14:45 -0700

    modified per Cory's feedback

--- practice/completer/site.js
+++ practice/completer/site.js
@@ -6,36 +6,49 @@
  * completions will have keys as the completion and values as weights.
  */
 function Completer() {
-  this.completions = {};
+  this.completionsToWeights = {};
 }
 
-/**
- * modifyCompleter has these methods:
- *
- * addCompletion() adds a completion to the completions container (in-place)
- * removeCompletion() removes a completion from the container (in-place)
- * complete() returns an array of completions, highest weighted first
- * selectCompletion() increases the weight of a given completion (in-place)
- *
- */
 var modifyCompleter = {
+  /**
+   * addCompletion adds a completion to the completions container, in-place
+   *   The weighting defaults to 1 (i.e., this is the initial weight.)
+   * @param {string} str the string to be added to the completions list
+   */
   addCompletion: function(str) {
-    this.completions[str] = 1;
+    if (typeof this.completionsToWeights[str] === 'undefined') {
+    this.completionsToWeights[str] = 1;
+    }
   },
+  /**
+   * removeCompletion() removes a completion from the container (in-place)
+   * @param  {string} str the string to be removed from the completions list.
+   */
   removeCompletion: function(str) {
-    delete this.completions[str];
+    delete this.completionsToWeights[str];
   },
+  /**
+   *complete() returns an array of completions, highest weighted first
+   * @param  {string} prefix The prefix to match to.
+   * @return {list of strings}  A list of strings that match the prefix.
+   */
   complete: function(prefix) {
-    var re = RegExp('^' + prefix);
-    var matches = _.pickBy(this.completions,
-      function(v, k) {return re.test(k);});
-    var sortedMatches = _.map(_.orderBy(_.toPairs(matches),
-                                        function(o) {return o[1];}, 'desc'),
-                              function(o) {return o[0];});
-    return sortedMatches;
+    var matchPattern = RegExp('^' + prefix);
+    var matchesToWeights = _.pickBy(this.completionsToWeights,
+      function(weight, completion) {return matchPattern.test(completion);});
+    var matchesWeights = _.toPairs(matchesToWeights);
+    var orderedMatchesWeights = _.orderBy(matchesWeights,
+      function(o) {return o[1];}, 'desc');
+    var orderedMatches = _.map(orderedMatchesWeights,
+      function(o) {return o[0];});
+    return orderedMatches;
   },
+  /**
+   * selectCompletion() increases the weight of a given completion (in-place)
+   * @param  {string} str The prefix that was matched.
+   */
   selectCompletion: function(str) {
-    this.completions[str] += 1;
+    this.completionsToWeights[str] += 1;
   }
 };
 
