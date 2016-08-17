d6aad928dd1937418d2bf28a1a8734b610b05c99
Eric Chase <echase6@gmail.com>
2016-08-17 11:03:25 -0700

    made code a little more readable

--- practice/earthquake/site.js
+++ practice/earthquake/site.js
@@ -4,13 +4,16 @@ var MS_IN_WK = 1000 * 60 * 60 * 24 * 7;
 var DOT_SCALE = 50000;  /// Good number is 50000
 var DOT_COLOR = '200, 128, 128';  ///'R, G, B'
 
+/**
+ * Makes Atom linter happier about the ol functions.
+ */
 if (!window.ol) {
   var ol;
 }
 
 /**
  * Function to load the earthquake data.  Uses a Promise.
- * @return {[json]} [the data in JSON format]
+ * @return {[json]} The data in JSON format
  */
 function getEarthquakeJSON() {
   var url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
@@ -46,9 +49,9 @@ function createMap() {
 }
 
 /**
- * [createSource description]
- * @param  {[type]} map [description]
- * @return {[type]}     [description]
+ * Create an empty source to add the earthquate icons to.
+ * @param  {ol.Map}      The Map element that will hold the source.
+ * @return {ol.Source}   The source that will hold the icons.
  */
 function createSource(map) {
   var vectorSource = new ol.source.Vector();
@@ -58,9 +61,10 @@ function createSource(map) {
 }
 
 /**
- * Function to parse the JSON file and gather location, mag, and time/date
- * @param  {[JSON]} json [the data from the earthquake reporting service]
- * @return {[array]}      [array of arrays: [long, lat, mag, UTC]]
+ * Function to parse the JSON file and gather location, mag, and time/Date
+ *   Filters out data with null values.
+ * @param  {[JSON]}   The data from the earthquake reporting service
+ * @return {[array]}  Array of arrays: [long, lat, mag, UTC]
  */
 function scrapeEarthquakeData(json) {
   var data = _.map(json.features, function(feature) {
@@ -70,28 +74,44 @@ function scrapeEarthquakeData(json) {
             feature.properties.time
           ];
   });
+  data = _.filter(data, function(feature) {
+    return _.every(feature, function(item) {return item !== null;});
+  });
   return data;
 }
 
 /**
+ * Create an indiviual filled circle
+ *    Radius is a function of the earthquake magnitude, larger = bigger.
+ *    Opacity is a function of the age; new = more opaque.
+ * @param  {array} data    Array holding [lon, lat, mag, time]
+ * @return {ol.Feature}    The circle feature to be added to the collection.
+ */
+function createDot(data) {
+  var nowTime = Date.now();
+  var circle = new ol.geom.Circle(
+                ol.proj.fromLonLat([data[0], data[1]]),
+                DOT_SCALE * data[2],
+                'XY'
+              );
+  var circleFeature = new ol.Feature(circle);
+  var opacity = 1.0 - (nowTime - data[3]) / MS_IN_WK;  /// newer = opaquer
+  var colorString = 'rgba(' + DOT_COLOR + ',' + opacity + ' )';
+  circleFeature.setStyle(new ol.style.Style({
+    fill: new ol.style.Fill({color: colorString})
+  }));
+  return circleFeature;
+}
+
+/**
  * Function to create a collection of Icons.
- * @param {[array]} data [array of arrays:  [long, lat, mag, UTC]]
+ * @param {[array]}         Array of arrays:  [long, lat, mag, UTC]]
+ * @return {ol.Collection}  The collection element to be added to the source.
  */
 function addIcons(data) {
-  var nowTime = Date.now();
   var iconCollection = new ol.Collection();
   _.forEach(data, function(item) {
-    var circle = new ol.geom.Circle(
-                  ol.proj.fromLonLat([item[0], item[1]]),
-                  DOT_SCALE * item[2],
-                  'XY'
-                );
-    var circleFeature = new ol.Feature(circle);
-    var opacity = 1.0 - (nowTime - item[3]) / MS_IN_WK;  /// newer = opaquer
-    var colorString = 'rgba(' + DOT_COLOR + ',' + opacity + ' )';
-    circleFeature.setStyle(new ol.style.Style({
-      fill: new ol.style.Fill({color: colorString})
-    }));
+    var circleFeature = createDot(item);
     iconCollection.push(circleFeature);
   });
   return iconCollection;
@@ -99,8 +119,9 @@ function addIcons(data) {
 
 /**
  * Display the earthquakes on the map by its features.
- * @param  {[ol.Source]}   [The vector source element]
- * @param  {[array]} data [array of arrays [long, lat, mag, UTC]]
+ *   Clear it first in case this is called as an update.
+ * @param  {[ol.Source]}  The vector source element
+ * @param  {[array]}      Array of arrays [long, lat, mag, UTC]
  */
 function displayEarthquakes(source, data) {
   var iconCollection = addIcons(data);
@@ -110,7 +131,8 @@ function displayEarthquakes(source, data) {
 
 /**
  * Overall function that gathers data and displays earthquake icons.
- * @param  {[ol.Map]} map [Displayed map element]
+ *   Asychronously displays the earthquake icons using Promse - then.
+ * @param  {[ol.source]} source element for icons.
  */
 function runDisplayEarthquakes(source) {
   getEarthquakeJSON().
@@ -122,7 +144,7 @@ function runDisplayEarthquakes(source) {
 }
 
 /**
- * [The call-back function that updates the already existing map.]
+ * The call-back function that updates the already existing map.
 */
 function updateMap() {
   var map = $('#map').data('map');
@@ -131,7 +153,7 @@ function updateMap() {
 }
 
 /**
- * Handlers for zoom-in/zoom-out buttons
+ * Handlers for zoom-in/zoom-out buttons and 5-minute icon updates.
  * @param  {[ol.Map} map [map element]
  */
 function registerMapEventHandlers(map) {
@@ -146,7 +168,8 @@ function registerMapEventHandlers(map) {
     var zoom = view.getZoom();
     view.setZoom(zoom + 1);
   };
-  var intervalID = window.setInterval(updateMap, 10000);
+  var FIVE_MIN_IN_MS = 5 * 60 * 1000;
+  var intervalID = window.setInterval(updateMap, FIVE_MIN_IN_MS);
 }
 
 /**
