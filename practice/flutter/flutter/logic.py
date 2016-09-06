dae594bc9d9bf56a141bd829dda1aa6d6b895268
Eric Chase <echase6@gmail.com>
2016-09-06 10:49:52 -0700

    improvements to doctests

--- practice/flutter/flutter/logic.py
+++ practice/flutter/flutter/logic.py
@@ -4,10 +4,10 @@ from . import models
 
 
 def get_flutts():
-    """Return 10 Flutts for display.
+    """Return 10 Flutts for display, ordered by timestamp, newest on top.
 
-    >>> create_save_new_flutt('Wo', '2016-09-05T11:00')
-    Flutt(text:'Wo', date:'2016-09-05T11:00')
+    >>> models.Flutt(text='Wo', timestamp='2016-09-05T11:00').save()
+    >>> models.Flutt(text='He', timestamp='2016-09-05T10:00').save()
     >>> get_flutts()  # doctest: +NORMALIZE_WHITESPACE
     [Flutt(text:'Wo', date:datetime.datetime(2016, 9, 5, 18, 0, tzinfo=<UTC>)),
     Flutt(text:'He', date:datetime.datetime(2016, 9, 5, 17, 0, tzinfo=<UTC>))]
@@ -20,7 +20,9 @@ def get_flutts():
 def get_queried_flutts(string):
     """Return 10 Flutts filtered flutts for display.
 
-    >>> get_queried_flutts('o')
+    >>> models.Flutt(text='Wo', timestamp='2016-09-05T11:00').save()
+    >>> models.Flutt(text='He', timestamp='2016-09-05T10:00').save()
+    >>> get_queried_flutts('o')  # doctest: +NORMALIZE_WHITESPACE
     [Flutt(text:'Wo', date:datetime.datetime(2016, 9, 5, 18, 0, tzinfo=<UTC>))]
     """
     flutt_list = models.Flutt.objects.all().order_by('-timestamp')
@@ -34,8 +36,9 @@ def get_queried_flutts(string):
 def create_save_new_flutt(text, timestamp):
     r"""Create a new Flutt, and save to the database.
 
-    >>> create_save_new_flutt('He', '2016-09-05T10:00')
-    Flutt(text:'He', date:'2016-09-05T10:00')
+    >>> nf = create_save_new_flutt('He', '2016-09-05T10:00')
+    >>> models.Flutt.objects.get(id=nf.id)
+    Flutt(text:'He', date:datetime.datetime(2016, 9, 5, 17, 0, tzinfo=<UTC>))
     """
     new_flutt = models.Flutt(text=text, timestamp=timestamp)
     new_flutt.save()
